4ff4fac45e4cfe9bff3a780ffc7772729d8d46a4
DEV B <dev.b@pos-system.local>
2026-03-18 13:34:52 +0800

    Add ItemStockSummaryService from Week 1

---
+++ src/Services/ItemStockSummaryService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace POS\Services;
+
+use POS\Database\DB;
+
+class ItemStockSummaryService
+{
+    public function getCurrentStock(int $itemId): array
+    {
+        $item = DB::fetch(
+            "SELECT id, sku, name FROM items WHERE id = ?",
+            [$itemId]
+        );
+
+        if (!$item) {
+            return [
+                'item_id'     => $itemId,
+                'sku'         => '',
+                'item_name'   => '',
+                'stock_on_hand' => 0.00,
+            ];
+        }
+
+        return [
+            'item_id'       => (int)$item['id'],
+            'sku'           => $item['sku'],
+            'item_name'     => $item['name'],
+            'stock_on_hand' => 0.00,
+        ];
+    }
+}
